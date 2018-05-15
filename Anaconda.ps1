5854ea400c4222d98a0110c05e21ed1852bf43f6
Artur Perwenis <aperwenis@gmail.com>
2018-05-16 00:28:21 +0200

    Work in progress. Jupyter file added.

---
+++ Anaconda.ps1
@@ -0,0 +1,3 @@
+#Projects folder
+cd E:\Artur\Development\AnacondaProjects
+E:\Artur\Development\Anaconda3\pythonw.exe E:\Artur\Development\Anaconda3\cwp.py E:\Artur\Development\Anaconda3 E:\Artur\Development\Anaconda3\pythonw.exe E:\Artur\Development\Anaconda3\Scripts\anaconda-navigator-script.py
